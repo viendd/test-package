da240f62616458f1521cd2f9175513586d370812
viendd <viendd@viralsoft.vn>
2021-02-22 16:11:28 +0700

    create migration conversation

---
+++ database/migrations/2021_02_22_085854_create_conversations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedBigInteger('user_send_id');
+            $table->foreign('user_send_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('user_receive_id');
+            $table->foreign('user_receive_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversations');
+    }
+}
