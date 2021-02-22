789beab9d53b552abf11bc5a77d0334da5d15304
“ViralS-longnd” <longnd@viralsoft.vn>
2021-02-22 17:31:17 +0700

    add static pages

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -10,3 +10,5 @@
     </ul>
 </li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon la la-question'></i> {{ __('menu.category') }}</a></li>
+
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('page') }}'><i class='nav-icon la la-file-o'></i> <span>Static Page</span></a></li>
