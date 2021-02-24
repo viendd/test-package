31cd1b863cf715e23074dd18288ff628640b7440
viendd <viendd@viralsoft.vn>
2021-02-24 10:40:12 +0700

    update list article

--- resources/lang/en/article.php
+++ resources/lang/en/article.php
@@ -7,5 +7,15 @@ return [
     'intro_short' => 'Intro short',
     'content' => 'Content',
     'image' => 'Image',
-    'user_created' => 'User created'
+    'user_created' => 'User created',
+    'status' => 'Status',
+    'top_title' => 'Top author write most (approve)',
+    'list_title' => 'List article',
+    'amount' => 'Amount',
+    'author' => 'Author',
+    'draft' => 'Draft',
+    'published' => 'Published',
+    'approve' => 'Approve',
+    'pending' => 'Pending',
+    'reject' => 'Reject'
 ];
--- resources/lang/vi/article.php
+++ resources/lang/vi/article.php
@@ -7,5 +7,15 @@ return [
     'intro_short' => 'Giới thiệu ngắn',
     'content' => 'Nội dụng',
     'image' => 'Ảnh đại diện',
-    'user_created' => 'Người tạo'
+    'user_created' => 'Người tạo',
+    'status' => 'Trạng thái',
+    'top_title' => 'Top tác giả viết bài nhiều nhất (đã duyệt)',
+    'list_title' => 'Danh sách các bài viết',
+    'amount' => 'Số lượng',
+    'author' => 'Tác giả',
+    'draft' => 'Lưu nháp',
+    'published' => 'Công khai',
+    'approve' => 'Đã duyệt',
+    'pending' => 'Chờ duyệt',
+    'reject' => 'Không duyệt'
 ];
